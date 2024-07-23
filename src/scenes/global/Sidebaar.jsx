fc30c9b5d0c84252be9a1421ae4db754ffc55221
Abraham Mekuria <abrahammekuria73@gmail.com>
2024-07-23 14:59:16 +0300

    image

--- src/scenes/global/Sidebaar.jsx
+++ src/scenes/global/Sidebaar.jsx
@@ -107,10 +107,10 @@ const Sidebaar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Abraham Mekuria
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Tribute Admin
                 </Typography>
               </Box>
             </Box>
